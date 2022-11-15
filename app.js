87bcd03d4a67670f6453ac4cd32bd0a8183dab5d
Alex Chandan <chandanakela321@gmail.com>
2022-11-15 20:37:44 +0530

    date and month list arranged in array

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ setInterval(showClock, 1000);
 
 function showClock() {
     let date = new Date();
-    // date = new Date(12, 23, 4)
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
@@ -11,6 +10,7 @@ function showClock() {
     let todayDate = date.getDate();
     let month = date.getMonth();
     let year = date.getFullYear();
+    console.log(month)
     let format;
     if (h > 12) {
         h -= 12;
@@ -23,91 +23,24 @@ function showClock() {
     else {
         format = 'AM'
     }
-    switch (day) {
-        case 0:
-            day = 'Sunday'
-            break;
-        case 1:
-            day = 'Monday'
-            break;
-        case 2:
-            day = 'Tuesday'
-            break;
-        case 3:
-            day = 'Wednesday'
-            break;
-        case 4:
-            day = 'Thursday'
-            break;
-        case 5:
-            day = 'Friday'
-            break;
-        case 6:
-            day = 'Saturday'
-            break;
-        default:
-            break;
-    }
-    // let weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    // for (let days in weeks) {
-    //     day = days;
-    // }
 
-    switch (month) {
-        case 0:
-            month = 'Jan'
-            break;
-        case 1:
-            month = 'Feb'
-            break;
-        case 2:
-            month = 'Mar'
-            break;
-        case 3:
-            month = 'Apr'
-            break;
-        case 4:
-            month = 'May'
-            break;
-        case 5:
-            month = 'Jun'
-            break;
-        case 6:
-            month = 'Jul'
-            break;
-        case 7:
-            month = 'Aug'
-            break;
-        case 8:
-            month = 'Sep'
-            break;
-        case 9:
-            month = 'Oct'
-            break;
-        case 10:
-            month = 'Nov'
-            break;
-        case 11:
-            month = 'Dec'
-            break;
-        default:
-            break;
-    }
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
-    currentDate = `<div class="weekdays">${day}</div>
+    currentDate = `<div class="weekdays">${weekdays[day]}</div>
                             <div>
                                 <div class="ampm">${format}</div>
                                 <div class="hoursAndMinutes">${h}:${m}</div>
                                 <div class="second">${s}</div>
                             </div>
-                        <div class="date">${todayDate}-${month}-${year}</div>
+                        <div class="date">${todayDate}-${months[month]}-${year}</div>
                     </div>`
     let clock = document.querySelector('.clock');
     clock.innerHTML = currentDate;
-    // console.log(currentDate)
 
 }
 showClock();
